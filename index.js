370a9e665dc4896b489cbc558bdb377af2fb652b
菅原 <takanori.sugawara@gmail.com>
2018-07-13 23:42:45 +0900

    baseコード

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
 const { log } = require('@vue/cli-shared-utils')
 
-module.exports = (api) => {
-    api.registerCommand('build-watch', (...args) => {
-        log('Starting webpack in watch mode...');
-        api.configureWebpack(webpackConfig => {
-            webpackConfig.watch = true
-        });
-
-        api.service.run('build', ...args);
-    });
-};
+module.exports = api => {}
