2112f4f63fca51e85e49303a1341d7ec08c761ea
菅原 孝則 <takanori.sugawara@gmail.com>
2018-07-13 21:20:50 +0900

    templateもアップデート

--- generator/template/commentary/index.js
+++ generator/template/commentary/index.js
@@ -5,6 +5,17 @@ import ColorStory from './example/ColorBox.story'
 storiesOf('atoms/ColorBox', ColorBox)
     .add('赤の説明が出てるはず', { props: { color: 'red' }, detail: '赤だよ' })
     .add('青の説明', { props: { color: 'blue' }, detail: '青だよ' })
-    .addExample('説明用のコンポーネント配置してみる', ColorStory)
 
-setup(Vue)
+storiesOf('molecules/ColorBox3', ColorBox).addExample('説明用のコンポーネント配置してみる', ColorStory)
+storiesOf('molecules/ddd/ColorBox4', ColorBox)
+storiesOf('orgamisms/ColorBox4', ColorBox)
+storiesOf('templates/ColorBox4', ColorBox)
+
+setup(Vue, {
+    icons: {
+        atoms: require('./icon/atom.svg'),
+        molecules: require('./icon/molecule.svg'),
+        orgamisms: require('./icon/organism.svg'),
+        templates: require('./icon/template.svg'),
+    },
+})
